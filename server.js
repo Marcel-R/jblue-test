2bb2efa446b7cd57452a2b73a9ead1367138c303
Marcel Remmerts <marcelremmerts84@gmail.com>
2018-06-21 13:58:40 +0200

    show diff between production and acceptance

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello world from the jblue test application');
+  response.end('<h1>Hello world from the jblue acceptance test application</h1>');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
