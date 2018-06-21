aa2ff000e5957185f8241d903d4be773395086cc
Marcel-R <marcelremmerts84@gmail.com>
2018-06-21 16:31:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
   response.end('<h1>Hello world from the jblue test application</h1>');
 };
 var www = http.createServer(handleRequest);
