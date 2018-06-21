052551a967cb93629aa74cc764e20b48b7d48e48
Marcel-R <marcelremmerts84@gmail.com>
2018-06-21 16:36:31 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  response.writeHead(200, {'Content-Type': 'text/html'});
   response.end('<h1>Hello world from the jblue test application</h1>');
 };
 var www = http.createServer(handleRequest);
