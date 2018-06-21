28c2c372577bee16de317189b6960fc3b1a456b3
Marcel Remmerts <marcelremmerts84@gmail.com>
2018-06-21 14:21:00 +0200

    moar shiney

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('<h1>Hello world from the jblue acceptance test application</h1>');
+  // response.end('<h1>Hello world from the jblue acceptance test application</h1>');
+  response.end('<h1 style="text-align: center;margin-top:20%">Hello world from the jblue acceptance test application</h1>');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
